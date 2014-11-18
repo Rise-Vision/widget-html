32a1dd611f3e8035d13f127466d5e19d64597cb5
Stuart Lees <stuartlees@gmail.com>
2014-11-18 16:59:12 -0500

    [Settings] Integration of angular-ui-ace, removal of ace-build
    - Revised bower.son to remove rv-ace-build and instead set dependency on angular-ui-ace
    - Updated settings HTML to import from ace-builds and angular-ui-ace package, apply the directive and handy options, remove the previous JS of setting options on editor
    - Controller now defines `aceLoaded` function for initiating more options and event listeners and further logic for dealing with the editor
    - Added ace module as dependency to app
    - Updated gulp file to add dependencies to settings unit tests

--- src/settings/ctr-html-settings.js
+++ src/settings/ctr-html-settings.js
@@ -1,7 +1,24 @@
 angular.module("risevision.widget.html.settings")
   .controller("htmlSettingsController", ["$scope", "$log",
-    function (/*$scope, $log*/) {
+    function ($scope, $log) {
 
+      $scope.aceLoaded = function(_editor){
+
+        var _session = _editor.getSession();
+
+        // Options
+        /* The worker is live syntax checking which is not required, however, it's also not usable due to the dependency
+           file getting requested at run time in the Ace code. Hence, ensuring the session turns the creation
+           of the worker off
+         */
+        _session.setUseWorker(false);
+
+        // Events
+        _session.on("change", function(){
+          // TODO: may or may not need this
+          $log.info("editor change!");
+        });
+      };
 
     }])
   .value("defaultSettings", {
