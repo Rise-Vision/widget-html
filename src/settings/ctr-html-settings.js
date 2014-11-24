1bb51d1f22311b015e172dde11dc2cffe61cbbcd
Stuart Lees <stuartlees@gmail.com>
2014-11-24 16:49:31 -0500

    [Settings] Saving and retrieving
    - Adding `ng-model` directive to editor element to bind the value that gets applied via `setValue()` of the editor to the `html` property in `settings.additionalParams`
    - Controller updated to revise some variable names, add `html` as a prop to `additionalParams` object and $watch the `html` value and check for the scenario that no previous html saved so the default HTML can get applied
    - e2e tests updated to run a test for saving settings

--- src/settings/ctr-html-settings.js
+++ src/settings/ctr-html-settings.js
@@ -2,38 +2,46 @@ angular.module("risevision.widget.html.settings")
   .controller("htmlSettingsController", ["$scope", "$log", "$templateCache",
     function ($scope, $log, $templateCache) {
 
-      var initialLoad = true;
-
-      $scope.defaultHTML = $templateCache.get("html5.html");
+      var aceLoadComplete = false,
+        defaultHTML = $templateCache.get("html5.html"),
+        editor, session;
 
       $scope.aceLoaded = function(_editor){
 
-        // Unsure as to why "onLoad" fires twice, preventing this logic happening twice with an initialLoad flag
-        if (initialLoad) {
-          var _session = _editor.getSession();
+        // Unsure as to why "onLoad" fires twice, preventing this logic happening twice with an aceLoadComplete flag
+        if (!aceLoadComplete) {
+          editor = _editor;
+          session = editor.getSession();
 
           // Options
           /* The worker is live syntax checking which is not required, however, it's also not usable due to the dependency
            file getting requested at run time in the Ace code. Hence, ensuring the session turns the creation
            of the worker off
            */
-          _session.setUseWorker(false);
+          session.setUseWorker(false);
 
           // Events
-          _session.on("change", function(){
+          session.on("change", function(){
             // TODO: may or may not need this
             $log.info("editor change!");
           });
 
-          _session.setValue($scope.defaultHTML);
-
-          initialLoad = false;
+          aceLoadComplete = true;
         }
 
       };
 
+      $scope.$watch("settings.additionalParams.html", function (newUrl, oldUrl) {
+        if (newUrl === "" && typeof oldUrl === "undefined") {
+          // app has begun with no previously saved html
+          $scope.settings.additionalParams.html = defaultHTML;
+        }
+      });
+
     }])
   .value("defaultSettings", {
     params: {},
-    additionalParams: {}
+    additionalParams: {
+      html: ""
+    }
   });
