822cd68907d2142f9a494ae6c3d46edb25e3ef38
Stuart Lees <stuartlees@gmail.com>
2014-11-25 12:21:26 -0500

    Inject HTML into iframe
    - widget HTML updated to remove unnecessary import of jQuery and widget common code, embed new embedHTML.js file, and add build expression for e2e mock data
    - main.js updated to register/call params and apply logic for handling play/pause/stop
    - Added `embedHTML.js`, module for encapsulating all code for configuring the iframe, retrieving the html value stored in settings, injecting the HTML into the iframe, and play/pause/stop behaviour
    - Added mock-data for widget e2e and unit tests
    - Updated gulp file to include mock-data in "html:e2e" task and added necessary test files for unit testing

---
+++ src/widget/embedHTML.js
@@ -0,0 +1,98 @@
+/* global gadgets */
+
+var RiseVision = RiseVision || {};
+RiseVision.EmbedHTML = {};
+
+RiseVision.EmbedHTML = (function (document, gadgets) {
+
+  "use strict";
+
+  // private variables
+  var _prefs = null,
+    _html = "",
+    _htmlInjected = false;
+
+  function _ready() {
+    gadgets.rpc.call("", "rsevent_ready", null, _prefs.getString("id"), true, true, true, true, true);
+  }
+
+  function _configureFrame() {
+    var container = document.getElementById("html-container"),
+      frame = document.getElementById("html-frame"),
+      aspectRatio =  (_prefs.getInt("rsH") / _prefs.getInt("rsW")) * 100;
+
+    // set the padding-bottom with the aspect ratio % (responsive)
+    if (container) {
+      container.setAttribute("style", "padding-bottom:" + aspectRatio + "%");
+    }
+
+    if (frame) {
+      frame.setAttribute("scrolling", "yes");
+    }
+  }
+
+  function _injectHTML() {
+    var frame = document.getElementById("html-frame");
+
+    if (frame) {
+      frame.contentWindow.document.open();
+      frame.contentWindow.document.write(_html);
+      frame.contentWindow.document.close();
+
+      _htmlInjected = true;
+    }
+  }
+
+  function _removeHTML() {
+    var frame = document.getElementById("html-frame");
+
+    if (frame) {
+      frame.contentWindow.document.open();
+      frame.contentWindow.document.write("");
+      frame.contentWindow.document.close();
+
+      _htmlInjected = false;
+    }
+  }
+
+  function _pause() {
+    _removeHTML();
+  }
+
+  function _play() {
+    if (!_htmlInjected) {
+      _injectHTML();
+    }
+  }
+
+  function _stop() {
+    _removeHTML();
+  }
+  function _setParams(names, values) {
+    var value;
+
+    _prefs = new gadgets.Prefs();
+
+    if (Array.isArray(names) && names.length > 0 && names[0] === "additionalParams") {
+      if (Array.isArray(values) && values.length > 0) {
+        value = JSON.parse(values[0]);
+
+        _configureFrame();
+
+        if (value.hasOwnProperty("html")) {
+          _html = value.html;
+        }
+
+        _ready();
+      }
+    }
+  }
+
+  return {
+    setParams: _setParams,
+    pause: _pause,
+    play: _play,
+    stop: _stop
+  };
+
+})(document, gadgets);
---
+++ test/mock-data.js
@@ -0,0 +1,15 @@
+(function(window) {
+
+  "use strict";
+
+  window.gadget = window.gadget || {};
+
+  window.gadget.settings = {
+    "params": {
+    },
+    "additionalParams": {
+      "html": "Hello World!"
+    }
+  };
+
+})(window);
